c0412450c6e405e1bf96b048013d40e199818a6f
Sana Khuram <sana.khuram.baig@gmail.com>
2025-01-25 11:06:04 +0100

    Removed console from spinner 🕸️

--- src/js/utilities/spinner.js
+++ src/js/utilities/spinner.js
@@ -1,21 +1,20 @@
 let spinnerTimeout;
 
 export function showSpinner(timeout = 15000) {
-  const spinner = document.getElementById("spinner");
+  const spinner = document.getElementById('spinner');
   if (spinner) {
-    spinner.classList.remove("hidden");
+    spinner.classList.remove('hidden');
 
     spinnerTimeout = setTimeout(() => {
       hideSpinner();
-      console.warn("Spinner timeout reached. Hiding spinner.");
     }, timeout);
   }
 }
 
 export function hideSpinner() {
-  const spinner = document.getElementById("spinner");
+  const spinner = document.getElementById('spinner');
   if (spinner) {
-    spinner.classList.add("hidden");
+    spinner.classList.add('hidden');
 
     if (spinnerTimeout) {
       clearTimeout(spinnerTimeout);
